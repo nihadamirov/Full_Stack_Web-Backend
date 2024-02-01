7729743b5e73db2ce2e91ae343df5674614f1712
Nihad <nihadamirov98@gmail.com>
2024-02-01 23:39:23 +0400

    Add model

---
+++ src/main/java/com/company/fullstackweb/FullStackWebApplication.java
@@ -0,0 +1,13 @@
+package com.company.fullstackweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/fullstackweb/FullStackWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.fullstackweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
